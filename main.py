58f68d877636346d984bc18ade69d8dd6630d9b9
Rick <ricgarridoc@hotmail.com>
2026-02-02 13:09:07 -0600

    Learning phyton

---
+++ main.py
@@ -0,0 +1,79 @@
+# Este es un comentario
+print("Hola mundo desde un archivo de python")
+
+"""
+Este es un comentario largo, debes usar "triples comillas dobles"
+estoy
+escribiendo
+más
+código
+
+
+Nomenclaturas
+SNAKE_CASE
+CamellCase
+
+Comillas dobles y comillas simples
+
+TIPOS DE DATOS
+-string
+-int (entero)
+-float (decimales)
+-boleano (boolean)
+
+CONSTANTES= "no existen"
+SOlo se pone el nombre en mayusculas y se toma como "de lectura", así CONSTANTE = 3.14, pero sigue siendo una variable
+
+Numeros enteros y decimales
+Siempre que uses "/" habrá como resultado un número flotante
+
+OPERADORES
+==, >, <, >=, <=, !=
+
+OPERADORES
+and, or, not
+
+
+"""
+
+nombre = "ric"
+nombre = "ricardo"
+Nombre = "Ricardo"
+edad = 39
+nombre_apellido_paterno = "Ricardo Garrido"
+nombre_completo = 'Ricardo "Garrido" Cruz'
+millones = -200_00_00
+flotante = -3.1416
+verdadero = True
+falso = False
+num1 = 10
+resultado = num1 + 10
+resultado_nuevo = num1 / 10
+flotante_grande = -100_000_000.00
+numero_uno = 10
+numero_dos = 20
+numero_tres = 30
+resultado_numeros = numero_uno == numero_uno
+resultado_dos = numero_uno < numero_dos and numero_uno < numero_tres
+resultado_tres = numero_uno >numero_dos and numero_uno < numero_tres
+
+
+print(Nombre)
+# "type()" Te dice el tipo de dato
+print(type(nombre))
+print(type(edad))
+print(nombre_apellido_paterno)
+print(nombre_completo)
+print(millones)
+print(type(millones))
+print(flotante)
+print(type(flotante))
+print(type(verdadero))
+print(verdadero)
+print(resultado)
+print(type(resultado))
+print(type(resultado_nuevo))
+print(flotante_grande)
+print(type(flotante_grande))
+print(type(resultado_numeros))
+print(resultado_dos)
