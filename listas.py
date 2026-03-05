912f4b1d8ccab859d7059531ef40399d21fbe58e
proferick <ricgarridoc@hotmail.com>
2026-03-04 21:30:20 -0600

    learning sublist

--- listas.py
+++ listas.py
@@ -1,6 +1,7 @@
 """
 LISTAS: son como arrays
 Recomendaciones: Guardar mismos tipos de datos en una lista
+Sublistas: es cortar una parte de una lista
 
 my_list = [1,"hola",True,[1,2,3]]
 print(my_list)
@@ -9,6 +10,19 @@ print(type(my_list))
 #           0       1       2       3       4   
 #           -5      -4      -3      -2      -1
 courses =["Python","Django","Flask","Ruby","MongoDB"]
+Python nos ofrece lectura de listas con números positivos y negativos
+value_nuevo= courses[-1]
+
+Sublistas: es cortar una parte de una lista, pones desde donde quieres que empiece a leer
+y donde quieres que termine de leer
+sublista = courses[0:3]
+sublista = courses[:3] #es lo mismo, python lo detecta
+#Puede empezar a mostar desde donde tu le digas
+sublista_2 = courses[3:5]
+#también puedes eludir el último número
+sublista_3 = courses[3:]
+# Y también puedes hacer una copia exacta, se le llama shallow copy
+sublista_4 = courses[:]
 
 """
 
@@ -27,3 +41,13 @@ print(len(courses))
 print(last_index)
 print(value)
 print(value_nuevo)
+
+sublista = courses[:3]
+sublista_2 = courses[2:4]
+sublista_3 = courses[3:]
+sublista_4 = courses[:]
+print(sublista)
+print(type(courses))
+print(sublista_2)
+print(sublista_3)
+print(sublista_4)
